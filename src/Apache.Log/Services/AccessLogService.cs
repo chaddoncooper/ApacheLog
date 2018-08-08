a27f8aa62b2e59b68dff8d2d8f45a3d0343b684b
Chad Cooper <spazmochad@gmail.com>
2018-08-08 15:57:19 +0100

    Beginning of API

---
+++ src/Apache.Log/Services/AccessLogService.cs
@@ -0,0 +1,33 @@
+﻿using Apache.Log.AccessLog;
+using Apache.Log.Hacker;
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+
+namespace Apache.Log
+{
+    public interface IAccessLogService
+    {
+        IEnumerable<string> GetAllUnidentifiedResourceRequestsSince(DateTime startDateTime, string path);
+    }
+
+    public class AccessLogService : IAccessLogService
+    {
+        private readonly IFinder _accessLogFinder;
+        private readonly IIdentifier _identifier;
+
+        public AccessLogService(IFinder accessLogFinder, IIdentifier identifier)
+        {
+            _accessLogFinder = accessLogFinder;
+            _identifier = identifier;
+        }
+
+        public IEnumerable<string> GetAllUnidentifiedResourceRequestsSince(DateTime startDateTime, string path)
+        {
+            var files = _accessLogFinder.GetLogFilesCreatedOnOrAfter(startDateTime, path);
+
+            var unidentifiedResourceRequests = new List<string>();
+            return unidentifiedResourceRequests;
+        }
+    }
+}

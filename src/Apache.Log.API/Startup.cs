d171aedb873b3bbf7b390718a53e6d44f1304328
Chad Cooper <spazmochad@gmail.com>
2018-08-08 15:57:19 +0100

    Beginning of API

---
+++ src/Apache.Log.API/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Apache.Log.AccessLog;
+using Apache.Log.Hacker;
+using Apache.Log.Resource;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Apache.Log.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddTransient<IFinder, Finder>();
+            services.AddTransient<IParser, Parser>();
+            services.AddTransient<IIdentifier, Identifier>();
+
+            var whitelist = new List<string>();
+            Configuration.GetSection("Whitelist").Bind(whitelist);
+
+            services.AddSingleton<IWhitelist, new Whitelist(whitelist.AsQueryable() > ();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}

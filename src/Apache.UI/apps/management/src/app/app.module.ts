569bdd80d2f485c144c18aa7ec7dc6964db93daa
Chad Cooper <spazmochad@gmail.com>
2019-07-21 23:23:42 +0100

    Add ng-zorro, scaffold table

--- src/Apache.UI/apps/management/src/app/app.module.ts
+++ src/Apache.UI/apps/management/src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BlacklistModule } from '@apache/blacklist';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import { NZ_ICONS, NZ_I18N, en_GB } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+registerLocaleData(en);
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(
+  key => antDesignIcons[key]
+);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BlacklistModule],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_GB },
+    { provide: NZ_ICONS, useValue: icons }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/Apache.UI/libs/blacklist/src/lib/blacklist.module.ts
+++ src/Apache.UI/libs/blacklist/src/lib/blacklist.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlacklistComponent } from './containers/blacklist/blacklist.component';
 import { RouterModule } from '@angular/router';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
+    NgZorroAntdModule,
     RouterModule.forChild([
       {
         path: '',

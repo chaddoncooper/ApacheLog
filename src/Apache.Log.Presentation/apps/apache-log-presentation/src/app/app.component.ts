50565044fcc1fba47c0d583d54e1a0d6caf5473e
Chad Cooper <spazmochad@gmail.com>
2019-09-24 17:18:22 +0100

    Scaffold Nx Angular Workspace

---
+++ src/Apache.Log.Presentation/apps/apache-log-presentation/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'apache-log-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'apache-log-presentation';
+}

90684bd5811a632163d8b7807fd4f9e3d579ae58
Chad Cooper <spazmochad@gmail.com>
2019-09-25 10:15:18 +0100

    Move layout to seperate component

--- src/Apache.Log.Presentation/apps/apache-log-presentation/src/app/app.module.ts
+++ src/Apache.Log.Presentation/apps/apache-log-presentation/src/app/app.module.ts
@@ -1,21 +1,27 @@
-
-
-registerLocaleData(en);import { BrowserModule } from '@angular/platform-browser';
+registerLocaleData(en);
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { IconsProviderModule } from './icons-provider.module';
-import { NgZorroAntdModule, NZ_I18N, en_GB } from 'ng-zorro-antd';
+import { NZ_I18N, en_GB } from 'ng-zorro-antd';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { LayoutModule } from '@apache-log/layout';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, IconsProviderModule, NgZorroAntdModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    LayoutModule
+  ],
   providers: [{ provide: NZ_I18N, useValue: en_GB }],
   bootstrap: [AppComponent]
 })
---
+++ src/Apache.Log.Presentation/libs/layout/src/lib/containers/main-layout/main-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'apache-log-main-layout',
+  templateUrl: './main-layout.component.html',
+  styleUrls: ['./main-layout.component.scss']
+})
+export class MainLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

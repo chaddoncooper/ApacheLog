b6d7f39c50e5cde1a0eefe6ff8547c2cf4f8f5b9
Chad Cooper <spazmochad@gmail.com>
2019-09-27 12:48:56 +0100

    Scaffold overview component

--- src/Apache.Log.Presentation/libs/dashboard/src/lib/dashboard.module.ts
+++ src/Apache.Log.Presentation/libs/dashboard/src/lib/dashboard.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OverviewComponent } from './containers/overview/overview.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [OverviewComponent]
 })
 export class DashboardModule {}

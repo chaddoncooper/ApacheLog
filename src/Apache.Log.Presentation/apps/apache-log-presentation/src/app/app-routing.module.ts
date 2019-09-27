99f5d0d255919be13c34aeacd360a48b55dbb587
Chad Cooper <spazmochad@gmail.com>
2019-09-27 13:01:47 +0100

    Add routing for dashboard overview

--- src/Apache.Log.Presentation/apps/apache-log-presentation/src/app/app-routing.module.ts
+++ src/Apache.Log.Presentation/apps/apache-log-presentation/src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  // { path: '', pathMatch: 'full', redirectTo: '/overview' },
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard/overview' },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('@apache-log/dashboard').then(m => m.DashboardModule)
+  },
   {
     path: 'blacklist',
     loadChildren: () =>
--- src/Apache.Log.Presentation/libs/dashboard/src/lib/dashboard.module.ts
+++ src/Apache.Log.Presentation/libs/dashboard/src/lib/dashboard.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverviewComponent } from './containers/overview/overview.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        redirectTo: 'overview'
+      },
+      {
+        path: 'overview',
+        component: OverviewComponent
+      }
+    ])
+  ],
   declarations: [OverviewComponent]
 })
 export class DashboardModule {}

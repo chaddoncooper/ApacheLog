30e6c4b5e35d1269e3a663b308af55683f926e8c
Chad Cooper <spazmochad@gmail.com>
2019-10-01 14:12:00 +0100

    Scaffold manage virtual hosts component

---
+++ src/Apache.Log.Presentation/libs/virtual-hosts/src/lib/containers/manage-virtual-hosts/manage-virtual-hosts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'apache-log-manage-virtual-hosts',
+  templateUrl: './manage-virtual-hosts.component.html',
+  styleUrls: ['./manage-virtual-hosts.component.scss']
+})
+export class ManageVirtualHostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/Apache.Log.Presentation/libs/virtual-hosts/src/lib/virtual-hosts.module.ts
+++ src/Apache.Log.Presentation/libs/virtual-hosts/src/lib/virtual-hosts.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ManageVirtualHostsComponent } from './containers/manage-virtual-hosts/manage-virtual-hosts.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [ManageVirtualHostsComponent]
 })
 export class VirtualHostsModule {}
